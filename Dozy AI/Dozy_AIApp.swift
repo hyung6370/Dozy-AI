3841df20ee9aaf2dda1d2f4a1b924f00d008be9f
KIM HyungJun <rlagudwns886@gmail.com>
2026-03-17 17:41:08 +0900

    Initial Commit

---
+++ Dozy AI/Dozy_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dozy_AIApp.swift
+//  Dozy AI
+//
+//  Created by HyungJun's mac on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Dozy_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
