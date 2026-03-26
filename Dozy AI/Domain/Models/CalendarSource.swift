c3bd7474f4d3b98dbb8ffcd0f51646a702e90901
Hyungjun KIM <rlagudwns886@gmail.com>
2026-03-26 16:34:23 +0900

    Feat: App Icon 추가 및 GoogleSignIn SPM 추가 (#5)

---
+++ Dozy AI/Domain/Models/CalendarSource.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarSource.swift
+//  Dozy AI
+//
+//  Created by Hyungjun KIM on 3/26/26.
+//
+
+import Foundation
+
+enum CalendarSource: String, Codable, CaseIterable, Hashable {
+    
+}
